1b8b0c8ce546867e4e073324a6118f21b86b94b9
Nguyễn Văn Toại <122630929+tojinguyen@users.noreply.github.com>
2025-10-16 17:07:10 +0700

    feat: update

--- ocr-project/app/db/session.py
+++ ocr-project/app/db/session.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 from app.core.config import settings
 
 engine = create_engine(settings.get_database_url(), pool_pre_ping=True)
+
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
